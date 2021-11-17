9951dbab3ff4dc3e961255ced7eff46e1f591623
王珏 <ou@ous-MacBook-Pro.local>
2021-11-16 23:38:33 -0500

    add some tests

--- test/token.js
+++ test/token.js
@@ -43,4 +43,76 @@ contract("DummyNFT", accounts => {
                 );
             })
     });
+
+    it("Test awardItem getApproved", () => {
+        let nft;
+
+        return DummyNFT.deployed()
+            .then(instance => {
+                nft = instance
+                return nft.awardItem(accounts[0], "Some URL", {from: accounts[0]})
+            })
+            .then(result => {
+                tokenId = result.logs[0].args.tokenId
+                return nft.getApproved(tokenId)
+            })
+            .then(operator => {
+                assert.equal(
+                    operator,
+                    accounts[0],
+                    "Account 0 doesn't get approved"
+                );
+            })
+    });
+
+    it("Test awardItem name", () => {
+        let nft;
+
+        return DummyNFT.deployed()
+            .then(instance => {
+                nft = instance
+                return nft.awardItem(accounts[0], "Some URL", {from: accounts[0]})
+            })
+            .then(result => {
+                tokenId = result.logs[0].args.tokenId
+                return nft.name()
+            })
+            .then(name => {
+                assert.equal(
+                    "Some URL",
+                    name,
+                    "No token collection name"
+                );
+            })
+    });
+
+    it("Test awardItem symbol", () => {
+        let nft;
+
+        return DummyNFT.deployed()
+            .then(instance => {
+                nft = instance
+                return nft.awardItem(accounts[0], "Some URL", {from: accounts[0]})
+            })
+            .then(result => {
+                tokenId = result.logs[0].args.tokenId
+                return nft.symbol()
+            })
+            .then(symbol => {
+                assert.equal(
+                    "Some URL",
+                    symbol,
+                    "No token collection symbol"
+                );
+            })
+    });
+
+
+
+
+
+    
+
+
+    
 });
